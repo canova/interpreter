3b73f469ad03e14956bf3803c13bd880db854e97
Nazım Can <canaltinova@users.noreply.github.com>
2015-10-18 21:44:20 +0300

    Added Lexer
    - First and basic version of the lexer added.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # basic-interpreter
 Basic Interpreter Written in Rust
+
+This project made during free times and for fun. The only purpose of the project is learning how to create an interpreter or a compiler.
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::env;
+
+mod lexer;
+
+fn main() {
+
+	let args: Vec<_> = env::args().collect();
+	let mut path = Path::new("src/test.c");
+
+    // For custom test source file for interpreting.
+	if args.len() > 1 {
+        println!("Your source file is: {}", args[1]);
+        path = Path::new(&*args[1]);
+    }
+
+	let display = path.display();
+	
+	let mut file = match File::open(&path) {
+    	// The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    let mut code = String::new();
+
+    match file.read_to_string(&mut code) {
+        Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
+        Ok(_) => print!("{} contains:\n{}", display, code),
+    }
+
+    let mut lexemes = lexer::Lexer::Initiate(code);
+
+    //We have the Tokens list now. Printing for debugging
+    for token in lexemes {
+	    println!("{:?}", token);
+	}
+}
