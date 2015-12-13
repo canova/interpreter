d3d5ebefd64fa0f21e6fa4d92c7414ce7a18e455
Nazım Can <canaltinova@users.noreply.github.com>
2015-12-13 02:05:03 +0200

    Updated AST
    - Updated Abstract Syntax Tree.

--- src/ast.rs
+++ src/ast.rs
@@ -5,11 +5,6 @@
  */
 
 use lexer::*;
-/*
-#[derive(Debug)]
-pub struct Program {
-    pub stmts: Vec<Expr>
-}
 
 #[derive(Debug)]
 pub struct Expr {
@@ -19,18 +14,28 @@ pub struct Expr {
 
 #[derive(Debug)]
 pub enum Expr_ {
+    // Add two expressions.
     Add(Box<Expr>, Box<Expr>),
+    // Subtract two expressions
     Sub(Box<Expr>, Box<Expr>),
+    // Multiply two expressions
     Mul(Box<Expr>, Box<Expr>),
+    // Divide two expressions
     Div(Box<Expr>, Box<Expr>),
+    // Variable expression
     Var(String),
+    // Assignment expression
     Assign(String, Box<Expr>),
+    // If expression 'if expr { expr } else { expr }'
+    If(Box<Expr>, Box<Expr>, Option<Box<Expr>>),
+    // Print expression
     Print(Box<Expr>),
-    Literal(i64),
-}*/
+    // Literal expression
+    Literal(i64)
+}
 
 pub enum Node {
-    OperatorNode {leftNode: Option<Box<Node>>, rightNode: Option<Box<Node>>},
+    OperatorNode {Children: Option<Vec<Node>>},
     NumberNode {value: i64},
     VariableNode {value: String},
     StringNode {value: String}
