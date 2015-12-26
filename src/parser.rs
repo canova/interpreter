08c9047ae2abb1e57398b756113130040ececfd6
Nazım Can <canaltinova@users.noreply.github.com>
2015-12-26 23:57:06 +0200

    Part of parseInteger function Added.
    - Part of the parseInteger function added. It is not complete yet.

--- src/parser.rs
+++ src/parser.rs
@@ -16,7 +16,7 @@ pub struct Parser {
 }
 
 impl Parser {
-    pub fn new(_tokenStream: TokenStream, _span: Option<Span>) -> Parser {
+    pub fn new(mut _tokenStream: TokenStream, _span: Option<Span>) -> Parser {
         let _tokenCount = _tokenStream.tokens.len();
         let _currentToken = _tokenStream.currentToken();
 
@@ -33,11 +33,11 @@ impl Parser {
         self.token.tokenType.toString()
     }
 
-    fn unexpectedToken(&self, c: char) { // TODO: Make more user friendly errors. It is temporary.
-        panic!("Unexpected token: {:?}", c);
+    fn unexpectedToken(&self, tt: TokenType) { // TODO: Make more user friendly errors. It is temporary.
+        panic!("Unexpected token found. Found: {:?}", tt);
     }
 
-    fn eatToken(&mut self, expectedToken: &TokenType) -> bool {
+    fn eatToken(&mut self, expectedToken: TokenType) -> bool {
         let isExist = self.checkToken(expectedToken);
 
         if isExist {
@@ -66,22 +66,52 @@ impl Parser {
     pub fn parse(&mut self) -> Expr {
 
         match self.token.tokenType {
-            TokenType::Keyword(String::from("int")) => Expr {span: None, node: self.parseInteger()},
-            TokenType::Keyword(String::from("string")) => Expr {span: None, node: self.parseString()},
-            TokenType::Keyword(String::from("print")) => Expr {span: None, node: self.parsePrint()},
+            TokenType::Keyword(ref x) if x == "int" => Expr {span: None, node: self.parseInteger()},
+            TokenType::Keyword(ref x) if x == "string" => Expr {span: None, node: self.parseString()},
+            TokenType::Keyword(ref x) if x == "print" => Expr {span: None, node: self.parsePrint()},
             _ => unimplemented!()
         }
     }
 
-    fn parseInteger(&self) {
+    fn parseInteger(&self) -> Expr_ {
+        let identifier : String;
+        let number : String;
+        let mut expr : Expr;
+
+        if self.eatToken(TokenType::Identifier) {
+            match self.token.tokenType {
+                TokenType::Identifier(ref x) => identifier = x
+            };
+
+            if self.eatToken(TokenType::Equals) {
+                if self.eatToken(TokenType::Number) {
+                    match self.token.tokenType {
+                        TokenType::Number(ref y) => {
+                            number = y;
+                            expr = Expr_::Assign {Identifier, Expr {span None, node: Expr_::Variable {number}}};
+                        };
+                    };
+
+                }
+            } else {
+                self.unexpectedToken(TokenType::Equals);
+            }
+        } else {
+            self.unexpectedToken(TokenType::Identifier);
+        }
+    }
+
+    fn parseString(&self) -> Expr_ {
         unimplemented!()
     }
 
-    fn parseString(&self) {
+    fn parsePrint(&self) -> Expr_ {
         unimplemented!()
     }
 
-    fn parsePrint(&self) {
-        unimplemented!()
+    fn expectSemicolon(&self) {
+        if self.eatToken(TokenType::Semicolon) {
+            self.unexpectedToken(TokenType::Semicolon);
+        }
     }
 }
